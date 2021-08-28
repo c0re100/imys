37f9145f8126fbe84e85cb5cee93d0713dedf5da
c0re100 <corehusky@gmail.com>
2021-08-28 10:29:56 +0800

    Implement Copy/Save canvas as image

--- index.js
+++ index.js
@@ -1,8 +1,73 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow, webFrameMain, clipboard} = require('electron')
+const contextMenu = require('electron-context-menu');
+const path = require("path");
+const nativeImage = require('electron').nativeImage
 
-function startIMYS() {
-    const window = new BrowserWindow({width: 1370, height: 900, webPreferences: {backgroundThrottling: false}})
-    window.loadURL('https://www.johren.games/games/imys-r-zh-tw/play/')
+contextMenu({
+    prepend: (defaultActions, parameters, browserWindow) => [
+        {
+            label: 'Copy image',
+            visible: parameters.mediaType === 'canvas',
+            click: (menuItem, browserWindow, event) => {
+                browserWindow.webContents.mainFrame.framesInSubtree.filter((frame) => {
+                    if (frame.url === 'https://johren-r18.irismystery.com/pc/iframe') {
+                        frame.executeJavaScript(`
+                            function takeScreenshot() {
+                                let canvas = document.querySelector('#unity-canvas');
+                                return canvas.toDataURL()
+                            }
+                            takeScreenshot()
+                        `).then((result) => {
+                            let b64 = nativeImage.createFromDataURL(result)
+                            clipboard.writeImage(b64)
+                        })
+                    }
+                })
+            }
+        },
+        {
+            label: 'Save image',
+            visible: parameters.mediaType === 'canvas',
+            click: (menuItem, browserWindow, event) => {
+                browserWindow.webContents.mainFrame.framesInSubtree.filter((frame) => {
+                    if (frame.url === 'https://johren-r18.irismystery.com/pc/iframe') {
+                        frame.executeJavaScript(`
+                            function saveImage() {
+                                let canvas = document.querySelector('#unity-canvas');
+                                canvas.toBlob((blob) => {
+                                  const a = document.createElement('a');
+                                  a.download = 'imys_screenshot_' + new Date().toLocaleString("en-CA", {hour12: false, timeZone: "Asia/Hong_Kong"}).
+                                        replace(/-/g, '_').
+                                        replace(/, /g, '_').
+                                        replace(/:/g, '_') + '.jpg';
+                                  a.href = URL.createObjectURL(blob);
+                                  document.body.appendChild(a);
+                                  a.click();
+                                  document.body.removeChild(a);
+                                });
+                            }
+                            saveImage()
+                        `)
+                    }
+                })
+            }
+        }
+    ]
+});
+
+async function startIMYS() {
+    const imysWindow = new BrowserWindow({
+        width: 1380,
+        height: 900,
+        webPreferences: {
+            backgroundThrottling: false,
+            contextIsolation: false,
+            nodeIntegrationInSubFrames: true,
+            preload: path.join(__dirname, 'unlocker.js'),
+        }
+    })
+
+    await imysWindow.loadURL('https://www.johren.games/games/imys-r-zh-tw/play/')
 }
 
-app.on("ready", startIMYS)
+app.on("ready", async () => startIMYS())
---
+++ unlocker.js
@@ -0,0 +1,99 @@
+(function() {
+    // Set preserveDrawingBuffer to true, so we can save canvas as image :)
+    if (typeof HTMLCanvasElement !== "undefined") {
+        wrapGetContext(HTMLCanvasElement);
+    }
+    if (typeof OffscreenCanvas !== "undefined") {
+        wrapGetContext(OffscreenCanvas);
+    }
+
+    function wrapGetContext(ContextClass) {
+        const isWebGL = /webgl/i;
+
+        ContextClass.prototype.getContext = function(origFn) {
+            return function(type, attributes) {
+                if (isWebGL.test(type)) {
+                    attributes = Object.assign({}, attributes || {}, {preserveDrawingBuffer: true});
+                }
+                return origFn.call(this, type, attributes);
+            };
+        }(ContextClass.prototype.getContext);
+    }
+
+    // Context menu is not allowed, so we need to re-enable it :)
+    // Source from https://greasyfork.org/scripts/23772-absolute-enable-right-click-copy/code/Absolute%20Enable%20Right%20Click%20%20Copy.user.js
+    var doc = document;
+
+    var docEvents = [
+        doc.oncontextmenu = null,
+        doc.onselectstart = null,
+        doc.ondragstart = null,
+        doc.onmousedown = null
+    ];
+
+    [].forEach.call(
+        ['copy', 'cut', 'paste', 'select', 'selectstart'],
+        function(event) {
+            document.addEventListener(event, function(e) { e.stopPropagation(); }, true);
+        }
+    );
+
+    window.addEventListener('contextmenu', function contextmenu(event) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        var handler = new eventHandler(event);
+        window.removeEventListener(event.type, contextmenu, true);
+        var eventsCallBack = new eventsCall(function() {});
+        handler.fire();
+        window.addEventListener(event.type, contextmenu, true);
+        if (handler.isCanceled && (eventsCallBack.isCalled)) {
+            event.preventDefault();
+        }
+    }, true);
+
+    function eventsCall() {
+        this.events = ['DOMAttrModified', 'DOMNodeInserted', 'DOMNodeRemoved', 'DOMCharacterDataModified', 'DOMSubtreeModified'];
+        this.bind();
+    }
+
+    eventsCall.prototype.bind = function() {
+        this.events.forEach(function (event) {
+            document.addEventListener(event, this, true);
+        }.bind(this));
+    };
+
+    eventsCall.prototype.handleEvent = function() {
+        this.isCalled = true;
+    };
+
+    eventsCall.prototype.unbind = function() {
+        this.events.forEach(function (event) {}.bind(this));
+    };
+
+    function eventHandler(event) {
+        this.event = event;
+        this.contextmenuEvent = this.createEvent(this.event.type);
+    }
+
+    eventHandler.prototype.createEvent = function(type) {
+        var target = this.event.target;
+        var event = target.ownerDocument.createEvent('MouseEvents');
+        event.initMouseEvent(
+            type, this.event.bubbles, this.event.cancelable,
+            target.ownerDocument.defaultView, this.event.detail,
+            this.event.screenX, this.event.screenY, this.event.clientX, this.event.clientY,
+            this.event.ctrlKey, this.event.altKey, this.event.shiftKey, this.event.metaKey,
+            this.event.button, this.event.relatedTarget
+        );
+        return event;
+    };
+
+    eventHandler.prototype.fire = function() {
+        var target = this.event.target;
+        var contextmenuHandler = function(event) {
+            event.preventDefault();
+        }.bind(this);
+        target.dispatchEvent(this.contextmenuEvent);
+        this.isCanceled = this.contextmenuEvent.defaultPrevented;
+    };
+}());
