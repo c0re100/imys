21febd40638ce1a98360e4cef9bf0d91d85f4337
c0re100 <corehusky@gmail.com>
2023-09-16 15:40:16 +0800

    Override Accept-Language value

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ async function startIMYS() {
 
     session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
         details.requestHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
+        details.requestHeaders['Accept-Language'] = 'ja'
         callback({ cancel: false, requestHeaders: details.requestHeaders });
     });
 
